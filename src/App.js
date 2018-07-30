5edca4a5f8e163efd2cf4773b8956f86381743de
jportella93 <jportella93@gmail.com>
2018-07-30 18:08:12 +0200

    feat: add boilerplate for container component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import PictureGrid from './containers/PictureGrid.jsx'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <PictureGrid/>
       </div>
     );
   }
---
+++ src/containers/PictureGrid.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PictureGrid extends Component {
+  render() {
+    return (
+      <h1>PictureGrid works!</h1>
+    )
+  }
+}
+
+export default PictureGrid
