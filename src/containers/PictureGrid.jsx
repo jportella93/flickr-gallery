88fbf927805a7221c00e7db5d40cd73f631dbaf9
jportella93 <jportella93@gmail.com>
2018-07-31 16:45:12 +0200

    fix: bug infinite loop on lazy loading

--- src/containers/PictureGrid.jsx
+++ src/containers/PictureGrid.jsx
@@ -7,6 +7,8 @@ import styled from 'styled-components';
 import Picture from '../components/Picture'
 import SelectedPicture from '../components/SelectedPicture'
 
+import throttle from 'lodash.throttle';
+
 const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
@@ -25,26 +27,34 @@ class PictureGrid extends Component {
   componentWillMount() {
     this.props.fetchPictures()
     this.props.fetchPictures()
-    this.props.fetchPictures()
+    this.lazyFetchPictures = throttle(this.props.fetchPictures, 500)
+    this.listenForScrollAndFetch()
   }
 
   // fetches new pictures when scrolled almost till the bottom
-  lazyFetchPictures = () => {
-    const innerH = window.innerHeight,
-          scrollY = window.scrollY,
-          docH = document.documentElement.scrollHeight
+  listenForScrollAndFetch = () => {
     window.addEventListener('scroll', (e) => {
-      if (innerH + scrollY >= docH - 100) this.props.fetchPictures()
+      if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - window.innerHeight/2) {
+        this.lazyFetchPictures()
+      }
     })
   }
 
+  selectPicture = (picture) => {
+    this.props.selectPicture(picture)
+  }
+
   render() {
-    const { pictures } = this.props;
-    this.lazyFetchPictures()
+    const { pictures, selectedPicture } = this.props;
 
     return (
       <Container>
-        {pictures && pictures.map(p => <Picture key={p.id} picture={p}/>)}
+        {pictures &&
+          pictures.map(p =>
+            <Picture key={p.id} picture={p} handleClick={this.selectPicture}/>)}
+        {selectedPicture &&
+          <SelectedPicture selectedPicture={selectedPicture}
+          handleClick={this.selectPicture} />}
       </Container>
     )
   }
