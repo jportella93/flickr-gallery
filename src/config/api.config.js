f444dfaaf84f88b9d39275d06ae1076952c66690
faishal <muhammadfaishal@xcidic.com>
2018-10-11 09:00:15 +0700

    change BASE_URL to default

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'http://localhost:8080'; // dev
-const BASE_URL = 'https://flickr-gallery-server.herokuapp.com'; // prod
+const BASE_URL = 'http://localhost:8080'; // dev
+// const BASE_URL = 'https://flickr-gallery-server.herokuapp.com'; // prod
 
 export {
   BASE_URL
